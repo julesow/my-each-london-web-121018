9dda565833e19ca9e226ba6a7ca66d87c6292050
SOULEYMANE SOW <souleymanesow@gmail.com>
2018-11-30 12:18:15 +0000

    Conflict resolved

--- my_each.rb
+++ my_each.rb
@@ -1,22 +1,14 @@
 def my_each (collection)# put argument(s) here
   # code here
-<<<<<<< HEAD
+
   
   i = 0
   while i < collection.length
  yield collection[i]
       i += 1
   end
-=======
-  if block_given?
-  i = 0
-  while i < collection.length
- collection[i]
-  yield  
-      i += 1
-  end
-else 
->>>>>>> 430a2813628e0c9c1697b0c9403c0ad3d511ea46
+
+
   
 collection
 end
